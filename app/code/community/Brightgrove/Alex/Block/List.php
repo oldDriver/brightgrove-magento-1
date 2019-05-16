bfb461a4ac0939396752a8a81f36e2c0af5c8eaa
oldDriver <alex.emelyanov.ua@gmail.com>
2019-05-16 10:54:08 +0300

    Some tricks

--- app/code/community/Brightgrove/Alex/Block/List.php
+++ app/code/community/Brightgrove/Alex/Block/List.php
@@ -4,5 +4,19 @@
 */
 class Brightgrove_Alex_Block_List extends Mage_Core_Block_Template
 {
-    
+    public function getQuote()
+    {
+        $qoute = Mage::getSingleton('checkout/session')->getQuote();
+        return $qoute;
+    }
+
+    public function getId()
+    {
+        $hlp1 = $this->helper('brightgrove_alex/array');
+        $hlp2 = Mage::helper('brightgrove_alex/array');
+        var_dump($hlp2::popArray([1,2,3,4,5]));
+        //$id = $this->getRequest()->getParam('id', '******');
+        $id = Mage::registry('asd');
+        return $id;
+    }
 }
--- app/code/community/Brightgrove/Alex/controllers/IndexController.php
+++ app/code/community/Brightgrove/Alex/controllers/IndexController.php
@@ -3,7 +3,18 @@ class Brightgrove_Alex_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
+        $id = $this->getRequest()->getParam('id');
+        
+        Mage::register('asd', '123');
         $this->loadLayout();
+        // Block
+        $block = $this->getLayout()->getBlock('bg_list');
+        // snake case to camel case
+        $block->setData('controller_id', $id);
+        $id++;
+        // create parameter
+        $block->setDummyControllerId($id);
+        
         $this->renderLayout();
         return $this;
     }
@@ -11,8 +22,12 @@ class Brightgrove_Alex_IndexController extends Mage_Core_Controller_Front_Action
     public function newAction()
     {
         $this->loadLayout();
+        $block = $this->getLayout()->getBlock('bg-');
+        $block->setData('test', 'asdsad');
         $this->renderLayout();
         return $this;
+        
+        
     }
 
     public function editAction()
